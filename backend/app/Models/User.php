3894cd7bc9061d5d700124cefddc52eb9700c5f3
bijit027 <debbijit70@gmail.com>
2026-06-11 20:28:25 +0600

    Fixed: User related bug

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -27,6 +27,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'trial_ends_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
