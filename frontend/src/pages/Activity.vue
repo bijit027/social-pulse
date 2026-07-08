82266bdea0c033476a4aa8e418cdec4245f6ae50
bijit027 <debbijit70@gmail.com>
2026-07-08 11:56:58 +0600

    test deploy

--- frontend/src/pages/Activity.vue
+++ frontend/src/pages/Activity.vue
@@ -59,7 +59,7 @@
             <div class="empty-state">
               <el-icon class="empty-icon"><Document /></el-icon>
               <h3>No activity found</h3>
-              <p>There are no recent events to display.</p>
+              <p>No recent events to show right now.</p>
             </div>
           </template>
         </el-table>
